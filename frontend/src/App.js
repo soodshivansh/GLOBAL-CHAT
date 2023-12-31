17cecdb3cbc8f5abaac6249257335f5033cde0fe
shivansh <soodshivansh5@gmail.com>
2023-12-31 16:37:59 +0530

    dpne

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const userAuthThroughServer = async (formData) => {
     try {
-      const response = await axios.post("http://localhost:4000/sendmessages", formData);
+      const response = await axios.post(`${BASE_URL}/sendmessages`, formData);
       const newMessage = response.data; // Assuming the server returns the new message
 
       // Update the local state with the new message
@@ -43,7 +43,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:4000/getmessages');
+        const response = await axios.get(`${BASE_URL}/getmessages`);
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
